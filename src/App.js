e734ccb6154071bac6e449796a1b7916dd2102a0
Eitan Sternlicht <eitanoffun@gmail.com>
2019-04-08 02:30:21 +0300

    added prettier and other editor stuff, formatted old code to prettier style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import { Dropzone } from "./components";
-import "./App.css";
+import React, { Component } from 'react';
+import { Dropzone } from './components';
+import './App.css';
 
 class App extends Component {
   render() {
