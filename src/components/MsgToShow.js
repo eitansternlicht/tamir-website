4311ca951347fb1a5604c483f6d4c4d200237203
bashbash <amjad.bashiti@hotmail.com>
2019-06-14 13:42:26 +0300

    updat the dialog of messages

--- src/components/MsgToShow.js
+++ src/components/MsgToShow.js
@@ -1,15 +1,29 @@
 
-import React, { useState } from "react";
+import React from "react";
 
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Button from '@material-ui/core/Button';
+import {
+    makeStyles,
+    Button,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    DialogContentText,
 
-const MsgToShow = ( {title, body, visible, handleClose}) => {
+} from '@material-ui/core/';
 
+const useStyles = makeStyles(theme => ({
+    title: {
+        textAlign: 'center',
+        fontSize: 30,
+    },
+
+
+}));
+
+const MsgToShow = ({ title, body, visible, handleClose }) => {
+
+    const classes = useStyles();
 
     function _handleClose() {
         handleClose();
@@ -17,14 +31,14 @@ const MsgToShow = ( {title, body, visible, handleClose}) => {
 
     return (
         <Dialog
-        disableBackdropClick
-      disableEscapeKeyDown
+            disableBackdropClick
+            disableEscapeKeyDown
             open={visible}
             onClose={_handleClose}
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
         >
-            <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+            <DialogTitle id="alert-dialog-title" className={classes.title}>{title}</DialogTitle>
             <DialogContent>
                 <DialogContentText id="alert-dialog-description">
                     {body}
